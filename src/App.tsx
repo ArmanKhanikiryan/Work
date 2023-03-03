d408734c1b0a9b4075c9b934c0fef6bca3754efb
ArmanKhanikiryan <akhanikiryan@gmail.com>
2023-03-03 19:05:13 +0400

    route map

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {FC} from "react";
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Home from "./pages/home";
@@ -6,15 +6,42 @@ import Navbar from "./components/navbar";
 import Products from "./pages/products";
 import Service from "./pages/service";
 
-function App() {
+export enum RoutesEnum {
+    HOME = "/",
+    Products = "/products",
+    Service = "/service",
+}
+
+export type RouteType = {
+    path: RoutesEnum;
+    Component: FC;
+};
+
+const RouteData:RouteType[] = [
+    {
+        path: RoutesEnum.HOME,
+        Component: Home,
+    },
+    {
+        path: RoutesEnum.Products,
+        Component: Products,
+    },
+    {
+        path: RoutesEnum.Service,
+        Component: Service,
+    }
+]
+
+const App = () => {
+
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/service" element={<Service/>} />
-      </Routes>
+        <Routes>
+        {RouteData.map(({path, Component}, index) => (
+            <Route key={index} path={path} element={<Component/>}/>
+        ))}
+        </Routes>
     </div>
   );
 }
